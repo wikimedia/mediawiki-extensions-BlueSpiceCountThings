f2677e1af90957ee4c9aa8f1bc96b0cf9fd57a8c
Peter Böhm <boehm@hallowelt.com>
2018-06-27 15:09:24 +0200

    Refactored Hook BSInsertMagicAjaxGetData - merge after https://gerrit.wikimedia.org/r/442296
    Change-Id: I89c69c8dcd9329ff99a8d86268776afdfb0f58f5

---
+++ src/Tag/CountArticlesHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BlueSpice\CountThings\Tag;
+
+use BlueSpice\Tag\Handler;
+
+class CountArticlesHandler extends Handler {
+
+	public function __construct( $processedInput, array $processedArgs, \Parser $parser, \PPFrame $frame ) {
+		parent::__construct( $processedInput, $processedArgs, $parser, $frame );
+	}
+
+	public function handle() {
+		return \CoreParserFunctions::numberofarticles( $this->parser );
+	}
+}
