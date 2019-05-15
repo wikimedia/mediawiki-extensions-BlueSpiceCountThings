7002bebba7782656d854adc19e277722fc6ace82
pwirth <wirth@hallowelt.biz>
2019-05-15 15:53:33 +0200

    Use renderer factory and new methods in renderer
    => Requires: I15bc2898248aa4272adf607ea8d0019b240910e2
    [3.1]
    
    Change-Id: I3f11516fb0a18b1f945440c461b7f07fffa69081

--- src/Renderer/CountCharacters.php
+++ src/Renderer/CountCharacters.php
@@ -2,6 +2,10 @@
 
 namespace BlueSpice\CountThings\Renderer;
 
+use Config;
+use IContextSource;
+use BlueSpice\Utility\CacheHelper;
+use MediaWiki\Linker\LinkRenderer;
 use BlueSpice\TemplateRenderer;
 use BlueSpice\Renderer\Params;
 
@@ -10,8 +14,26 @@ class CountCharacters extends TemplateRenderer {
 	const TITLE = 'title';
 	const TITLELINK = 'titlelink';
 
-	public function __construct( \Config $config, Params $params, LinkRenderer $linkRenderer = null ) {
-		parent::__construct( $config, $params, $linkRenderer );
+	/**
+	 * Constructor
+	 * @param Config $config
+	 * @param Params $params
+	 * @param LinkRenderer|null $linkRenderer
+	 * @param IContextSource|null $context
+	 * @param string $name | ''
+	 * @param CacheHelper|null $cacheHelper
+	 */
+	protected function __construct( Config $config, Params $params,
+		LinkRenderer $linkRenderer = null, IContextSource $context = null,
+		$name = '', CacheHelper $cacheHelper = null ) {
+		parent::__construct(
+			$config,
+			$params,
+			$linkRenderer,
+			$context,
+			$name,
+			$cacheHelper
+		);
 
 		$this->args[static::TITLE] = $params->get( static::TITLE, '' );
 		$this->args[static::TITLELINK] = $params->get( static::TITLELINK, '' );
