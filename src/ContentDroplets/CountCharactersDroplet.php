bfecc5ad1988aa824cfd94258776727518f971b4
hslater <hamish.slater@hallowelt.com>
2025-01-21 21:17:24 +0100

    Update Message namespace
    ERM38444
    
    [REL1_43, master]

--- src/ContentDroplets/CountCharactersDroplet.php
+++ src/ContentDroplets/CountCharactersDroplet.php
@@ -3,7 +3,7 @@
 namespace BlueSpice\CountThings\ContentDroplets;
 
 use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
-use Message;
+use MediaWiki\Message\Message;
 
 class CountCharactersDroplet extends TagDroplet {
 
